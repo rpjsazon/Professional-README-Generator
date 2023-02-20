5a2b4ab57de73beff16c4ce075ef9c4aae3fb7ae
Robert Patrick Jordan Sazon <rpjsazon@gmail.com>
2023-02-20 22:43:54 +0000

    question added

--- index.js
+++ index.js
@@ -2,10 +2,41 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+
 const questions = [
   {
     type:'input',
     name: 'title',
     message: 'What is the title of the Project?'
-  }
+  },
+  {
+    type:'input',
+    name: 'title',
+    message: 'What is the title of the Project?'
+  },
+  {
+    type:'input',
+    name: 'title',
+    message: 'What is the title of the Project?'
+  },
+  {
+    type:'input',
+    name: 'title',
+    message: 'What is the title of the Project?'
+  },
+  {
+    type:'input',
+    name: 'title',
+    message: 'What is the title of the Project?'
+  },
+  {
+    type:'input',
+    name: 'title',
+    message: 'What is the title of the Project?'
+  },
+  {
+    type:'input',
+    name: 'title',
+    message: 'What is the title of the Project?'
+  },
 ]

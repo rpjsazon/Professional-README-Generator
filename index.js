b5912889209d4252218caab75ce285bb9bf59eac
Robert Patrick Jordan Sazon <rpjsazon@gmail.com>
2023-02-20 22:38:20 +0000

    question added and global assignments added.

---
+++ index.js
@@ -0,0 +1,11 @@
+//Global assignments
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const questions = [
+  {
+    type:'input',
+    name: 'title',
+    message: 'What is the title of the Project?'
+  }
+]

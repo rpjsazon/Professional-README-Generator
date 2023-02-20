7a6e2a49c58cceee87c40abf780fd582576988f5
Robert Patrick Jordan Sazon <rpjsazon@gmail.com>
2023-02-20 23:05:54 +0000

    modified question and added initialize function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Global assignments
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 
 const questions = [
@@ -11,32 +12,43 @@ const questions = [
   },
   {
     type:'input',
-    name: 'title',
-    message: 'What is the title of the Project?'
+    name: 'description',
+    message: 'What is your project description?'
   },
   {
     type:'input',
-    name: 'title',
-    message: 'What is the title of the Project?'
+    name: 'installation',
+    message: 'How you install your project?'
   },
   {
     type:'input',
-    name: 'title',
-    message: 'What is the title of the Project?'
+    name: 'usage',
+    message: 'How you use your project?'
   },
   {
     type:'input',
-    name: 'title',
-    message: 'What is the title of the Project?'
+    name: 'contributing',
+    message: 'How should people contribute to this project?'
   },
   {
     type:'input',
-    name: 'title',
-    message: 'What is the title of the Project?'
+    name: 'tests',
+    message: 'How do you test this project?'
   },
   {
-    type:'input',
-    name: 'title',
-    message: 'What is the title of the Project?'
+    type:'checkbox',
+    name: 'license',
+    message: 'Choose a license for this project',
+    choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause Simplified License', 'None']
   },
 ]
+
+function init(){
+  inquirer.prompt(questions)
+  .then(function (userInput){
+    console.log(userInput)
+    
+  })
+}
+
+init();
